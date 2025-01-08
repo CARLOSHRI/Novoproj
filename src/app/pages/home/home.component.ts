d2bf8e8dd61d0867f11b9a9f3be1c5678f01a9d1
Carlos Henrique <carloshribeiro99@gmail.com>
2025-01-08 12:17:28 -0300

    components createds with styles

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { MenuTitleComponent } from "../../components/menu-title/menu-title.component";
+import { BigCardComponent } from "../../components/big-card/big-card.component";
+import { CardComponent } from "../../components/small/small-card/card.component";
+import { MenuBarComponent } from "../../components/menu-bar/menu-bar.component";
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [MenuTitleComponent, BigCardComponent, CardComponent, MenuBarComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
