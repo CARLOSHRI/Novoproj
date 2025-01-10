4daff6d24b6dd65353237279cc8fe50975e8a708
Carlos Henrique <carloshribeiro99@gmail.com>
2025-01-10 10:46:02 -0300

    components dinamycs propertys

--- src/app/components/small/small-card/card.component.ts
+++ src/app/components/small/small-card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -8,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrl: './card.component.css'
 })
 export class CardComponent {
+  @Input()
+  photoCover:string=""
+  @Input()
+  cardTitle:string=""
+
 
 }
