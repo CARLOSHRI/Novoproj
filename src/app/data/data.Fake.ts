63fe6fd14e16e414a8dc757642b625080fe8812d
Carlos Henrique <carloshribeiro99@gmail.com>
2025-01-11 11:53:36 -0300

    content created

---
+++ src/app/data/data.Fake.ts
@@ -0,0 +1,19 @@
+export const dataFake = [
+
+  {
+    "id":"1",
+    "title": "O novo filme do Dragon Ball é anunciado hoje.",
+    "description": "Hoje comemora o novo filme de Dragon Ball.",
+    "photoCover": "//tm.ibxk.com.br/2023/07/06/06125649938634.jpg"
+
+
+  },
+  {
+    "id":"2",
+    "title": "Saiu Novo filme do Dragon Ball",
+    "description": "ola",
+    "photoCover": "//imgsrv.crunchyroll.com/cdn-cgi/image/fit=contain,format=auto,quality=85,width=480,height=720/catalog/crunchyroll/257cdb6a1fdd566600a01eb0f9f73b22.jpg"
+
+
+  }
+]
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,15 +2,19 @@ import { Component } from '@angular/core';
 import { MenuTitleComponent } from "../../components/menu-title/menu-title.component";
 import { BigCardComponent } from "../../components/big-card/big-card.component";
 import { CardComponent } from "../../components/small/small-card/card.component";
-import { MenuBarComponent } from "../../components/menu-bar/menu-bar.component";
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MenuTitleComponent, BigCardComponent, CardComponent, MenuBarComponent],
+  imports: [MenuTitleComponent, BigCardComponent, CardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
+
+
+
 
 }
